bd2ff13d383f6c5614ec36b86d303d90356ad73d
RawDuck <mrseiseo@gmail.com>
2018-07-26 16:53:05 +0200

    Adding AbsoluteValueOfNumber

---
+++ C#/AbsoluteValue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Preparing variables///
+            Console.WriteLine("Give any number: ");
+            int num = Int32.Parse(Console.ReadLine());
+            
+            if(num < 0)
+            {
+                num = -num;
+            }
+
+            Console.WriteLine("Absolute value of your number: " + num);
+        }
+    }
+}
