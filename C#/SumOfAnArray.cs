617277c9dcf4e672d3dd7ba72879f8d1344c7a29
RawDuck <mrseiseo@gmail.com>
2018-07-26 16:45:05 +0200

    First tasks

---
+++ C#/SumOfAnArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Preparing array and variables///
+            Console.WriteLine("Give size of an array: ");
+            int arrsize = Int32.Parse(Console.ReadLine());
+
+            int[] array = new int[arrsize];
+            Random rnd = new Random();
+            int result = 0;
+
+            ///Filling array with random numbers///
+            for(int i=0; i<arrsize; i++)
+            {
+                array[i] = rnd.Next(1,100);
+                Console.WriteLine("el " + i + " = " + array[i]);
+            }
+
+            ///Summing array///
+            for(int i=0; i<arrsize; i++)
+            {
+                result += array[i];
+            }
+
+            Console.WriteLine("Sum of your array: " + result);
+        }
+    }
+}
